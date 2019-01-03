7261dd04b6f8c85e04c9cdf28e9b00d81f7363dc
Boyofthefuture <futureboydarren@gmail.com>
2019-01-03 16:05:05 +0800

    Tutorial 1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            movies: []
+        }
+    }
+    componentDidMount() {
+        let dataURL = "http://site.christtheking.com.sg/wp-json/wp/v2/movies?_embed";
+        fetch(dataURL)
+            .then(res => res.json())
+            .then(res => {
+                this.setState({
+                    movies: res
+                });
+            })
+    }
+
+    render() {
+        let movies = [];
+        if (Object.keys(this.state.movies).length === 0) { // check if movie object is empty
+            return <div>Data not loaded</div>;
+        } else {
+            movies = this.state.movies.map((movie, index) => {
+                return <div key={index}>
+                    <img src={movie._embedded['wp:featuredmedia'][0].media_details.sizes.full.source_url} alt="Star Warx Rulez" />
+                    <p><strong>Title:</strong> {movie.title.rendered}</p>
+                    <p><strong>Release Year:</strong> {movie.acf.release_year}</p>
+                    <p><strong>Rating:</strong> {movie.acf.rating}</p>
+                    <div><strong>Description:</strong><div dangerouslySetInnerHTML={ {__html: movie.acf.description} } /></div>
+                </div>
+            });
+        }
+
+
+        return (
+            <div>
+                <h2>Star Wars Movies</h2>
+                {movies}
+            </div>
+        )
+    }
 }
 
 export default App;
